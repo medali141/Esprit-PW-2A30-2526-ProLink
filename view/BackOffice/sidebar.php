53e0cd931092f5f955a7a7204e2825a7630c1e4c
ItsBat <82419654+medali141@users.noreply.github.com>
2026-04-15 14:41:49 +0100

    register +login + profile user

--- view/BackOffice/sidebar.php
+++ view/BackOffice/sidebar.php
@@ -8,8 +8,11 @@
         <ul>
             <li><a href="dashboard.php"><span class="icon">🏠</span> <span>Dashboard</span></a></li>
             <li><a href="listUsers.php"><span class="icon">👤</span> <span>Gestion Users</span></a></li>
-            <li><a href="#"><span class="icon">📁</span> <span>Gestion Projets</span></a></li>
+             <li><a href="#"><span class="icon">📁</span> <span>Gestion Projets</span></a></li>
             <li><a href="#"><span class="icon">📅</span> <span>Gestion Events</span></a></li>
+            <li><a href="profile_admin.php"><span class="icon">👤</span> <span>Mon profil (admin)</span></a></li>
+            <li><a href="../logout.php"><span class="icon">🔓</span> <span>Se déconnecter</span></a></li>
+           
         </ul>
     </nav>
 </div>
